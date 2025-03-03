bdeeea67307d9d712b1917a4190a6a3db0eaebd9
Augustin Cramer <acramer@hotmail.fr>
2025-03-03 12:45:12 +0100

    change version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ml_models',
-    version='0.1',
+    name='ml_models_augustin',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'numpy',
